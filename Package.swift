2600d5bc7cae5deca5cbfc512840486dd871bf9e
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-01-13 17:34:32 -0800

    Set platform requirements

--- Package.swift
+++ Package.swift
@@ -6,6 +6,15 @@ import PackageDescription
 
 let package = Package(
     name: "XCTestKit",
+    platforms:
+    [
+        .iOS(.v18),
+        .macCatalyst(.v18),
+        .macOS(.v15),
+        .tvOS(.v18),
+        .visionOS(.v2),
+        .watchOS(.v11)
+    ],
     products:
     [
         .library(
