4f47c8465d5a43615b270d4e2ccacdb7fc7d6920
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-01-19 16:19:36 -0800

    Add `Array<DiffNode>.isSetTree`

---
+++ Sources/XCTestKit/Utilities/Extensions/Array+DiffNode.swift
@@ -0,0 +1,32 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the XCTestKit open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+internal extension Array<DiffNode>
+{
+    /// Whether the diff tree represents a set.
+    var isSetTree: Bool
+    {
+        guard !self.isEmpty
+        else
+        {
+            return false
+        }
+        
+        for node in self
+        {
+            guard case .member = node.label
+            else
+            {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
