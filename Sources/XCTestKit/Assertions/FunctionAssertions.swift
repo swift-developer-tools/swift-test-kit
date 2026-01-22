549c4cbf44b1290c88413d6a19769623e0b16e61
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-01-22 11:18:31 -0800

    Add `nil` and non-`nil` function assertions

--- Sources/XCTestKit/Assertions/FunctionAssertions.swift
+++ Sources/XCTestKit/Assertions/FunctionAssertions.swift
@@ -137,3 +137,167 @@ public func XCTKAssertFalse(
         )
     }
 }
+
+
+
+// MARK: - Nil and non-nil
+
+/// Asserts that the given expression is `nil`.
+///
+/// This generates a failure when `expression != nil`.
+///
+/// - Parameters:
+///   - expression: An expression of type `Any?` to compare against `nil`.
+///   - message: An optional description of a failure.
+///   - file: The file where the failure occurs. The default value is the
+///   filename of the test case in which this function was called.
+///   - line: The line where the failure occurs. The default value is the line
+///   number where this function was called.
+public func XCTKAssertNil(
+    _ expression    : @autoclosure () throws -> Any?,
+    _ message       : @autoclosure () -> String         = "",
+    file            : StaticString                      = #filePath,
+    line            : UInt                              = #line
+)
+{
+    let value: Any?
+    
+    do
+    {
+        value = try expression()
+    }
+    catch
+    {
+        failAssertion(
+            kind:       .`nil`,
+            reason:     "threw error \"\(error)\"",
+            message:    message,
+            file:       file,
+            line:       line
+        )
+        
+        return
+    }
+    
+    if value != nil
+    {
+        failAssertion(
+            kind:       .`nil`,
+            reason:     nil,
+            message:    message,
+            file:       file,
+            line:       line
+        )
+    }
+}
+
+
+
+/// Asserts that the given expression is not `nil`.
+///
+/// This generates a failure when `expression == nil`.
+///
+/// - Parameters:
+///   - expression: An expression of type `Any?` to compare against `nil`.
+///   - message: An optional description of a failure.
+///   - file: The file where the failure occurs. The default value is the
+///   filename of the test case in which this function was called.
+///   - line: The line where the failure occurs. The default value is the line
+///   number where this function was called.
+public func XCTKAssertNotNil(
+    _ expression    : @autoclosure () throws -> Any?,
+    _ message       : @autoclosure () -> String         = "",
+    file            : StaticString                      = #filePath,
+    line            : UInt                              = #line
+)
+{
+    let value: Any?
+    
+    do
+    {
+        value = try expression()
+    }
+    catch
+    {
+        failAssertion(
+            kind:       .notNil,
+            reason:     "threw error \"\(error)\"",
+            message:    message,
+            file:       file,
+            line:       line
+        )
+        
+        return
+    }
+    
+    if value == nil
+    {
+        failAssertion(
+            kind:       .notNil,
+            reason:     nil,
+            message:    message,
+            file:       file,
+            line:       line
+        )
+    }
+}
+
+
+
+/// Asserts that the given expression is not `nil`, and returns the unwrapped
+/// value.
+///
+/// This generates a failure when `expression == nil`. Otherwise, it returns
+/// the unwarpped value of `expression`.
+///
+/// - Parameters:
+///   - expression: An expression of type `T?`.
+///   - message: An optional description of a failure.
+///   - file: The file where the failure occurs. The default value is the
+///   filename of the test case in which this function was called.
+///   - line: The line where the failure occurs. The default value is the line
+///   number where this function was called.
+/// - Returns: The result of evaluating and unwrapped the given expression.
+/// This only returns a value if the unwrapped value is not `nil`.
+/// - Throws: An ``XCTKUnwrapError`` if the unwrapped value is `nil`.
+public func XCTKUnwrap<T>(
+    _ expression    : @autoclosure () throws -> T?,
+    _ message       : @autoclosure () -> String     = "",
+    file            : StaticString                  = #filePath,
+    line            : UInt                          = #line
+) throws -> T
+{
+    let value: T?
+    
+    do
+    {
+        value = try expression()
+    }
+    catch
+    {
+        failAssertion(
+            kind:       .unwrap,
+            reason:     "threw error \"\(error)\"",
+            message:    message,
+            file:       file,
+            line:       line
+        )
+        
+        throw error
+    }
+    
+    if let unwrapped: T = value
+    {
+        return unwrapped
+    }
+    
+    failAssertion(
+        kind:       .unwrap,
+        reason:     nil,
+        message:    message,
+        file:       file,
+        line:       line
+    )
+    
+    throw XCTKUnwrapError()
+}
