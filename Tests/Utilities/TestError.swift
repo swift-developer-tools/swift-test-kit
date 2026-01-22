b5ecde4cc929aaa54cabc0c0dc970b0f1e31620f
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-01-22 08:04:38 -0800

    Add `TestError` test utility

---
+++ Tests/Utilities/TestError.swift
@@ -0,0 +1,18 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the XCTestKit open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+/// An error used to test assertions that accept a throwing expression.
+struct TestError: Error
+{
+    /// Throws a ``TestError``.
+    static func throwError() throws -> Bool
+    {
+        throw TestError()
+    }
+}
