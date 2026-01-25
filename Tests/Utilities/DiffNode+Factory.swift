b6689f66f7bb6495956eb6ccb953f2322d3d050b
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-01-25 10:28:34 -0800

    Add `DiffNode.makeStructuralAny()` test utility

--- Tests/Utilities/DiffNode+Factory.swift
+++ Tests/Utilities/DiffNode+Factory.swift
@@ -87,6 +87,36 @@ extension DiffNode
     
     
     
+    /// Creates a different structural node from the given values.
+    ///
+    /// Use this when the expected and actual values cannot conform to
+    /// `Equatable` (for example, tuples).
+    ///
+    /// - Parameters:
+    ///   - label: The diff node label.
+    ///   - expected: The expected value.
+    ///   - actual: The actual value.
+    ///   - tree: The diff nodes.
+    /// - Returns: The different structural node.
+    static func makeStructuralAny(
+        label       : DiffNodeLabel,
+        expected    : Any,
+        actual      : Any,
+        tree        : [DiffNode]
+    ) -> DiffNode
+    {
+        return DiffNode(
+            label:  label,
+            kind:   .different(
+                        expected:   DiffValue(expected),
+                        actual:     DiffValue(actual),
+                        tree:       tree
+                    )
+        )
+    }
+    
+    
+    
     /// Creates a cycle node from the given values.
     /// - Parameters:
     ///   - label: The diff node label.
