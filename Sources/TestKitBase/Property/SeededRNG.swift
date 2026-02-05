e332d5e3fe60c9da3ae45f283dcb8f6b5796a723
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-02-05 14:58:09 -0800

    Prevent overflow in `SeededRNG.next()`
    Unsigned arithmetic implicitly wraps on overflow in C. Use overflow
    operators to avoid trapping.

--- Sources/TestKitBase/Property/SeededRNG.swift
+++ Sources/TestKitBase/Property/SeededRNG.swift
@@ -36,12 +36,12 @@ internal struct SeededRNG: RandomNumberGenerator
     /// - Returns: The next random value.
     public mutating func next() -> UInt64
     {
-        state += 0x9e3779b97f4a7c15
+        state &+= 0x9e3779b97f4a7c15
         
         var next: UInt64 = state
         
-        next = (next ^ (next >> 30)) * 0xbf58476d1ce4e5b9
-        next = (next ^ (next >> 27)) * 0x94d049bb133111eb
+        next = (next ^ (next >> 30)) &* 0xbf58476d1ce4e5b9
+        next = (next ^ (next >> 27)) &* 0x94d049bb133111eb
         
         return next ^ (next >> 31)
     }
