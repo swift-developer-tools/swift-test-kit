f055becd5ba19b359d2c4cf64770db41da95171a
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-02-07 12:00:31 -0800

    Add `FixedWidthInteger` shrink-within-range support

--- Sources/TestKitBase/Property/Extensions/FixedWidthInteger+Shrink.swift
+++ Sources/TestKitBase/Property/Extensions/FixedWidthInteger+Shrink.swift
@@ -9,29 +9,77 @@
 
 internal extension FixedWidthInteger
 {
-    /// Shrinks the value toward zero by repeatedly halving the distance.
+    /// Shrinks the value toward zero or the nearest bound by repeatedly
+    /// halving the distance.
+    ///
+    /// Shrink candidates converage toward zero if zero is within the range,
+    /// otherwise toward the nearest bound.
+    ///
+    /// - Parameter range: The range in which to generate integers.
     /// - Returns: The shrink candidates.
-    func shrinkTowardZero() -> [Self]
+    func shrinkTowardZero(
+        in range: ClosedRange<Self>? = nil
+    ) -> [Self]
     {
-        guard self != 0
+        guard
+            let range,
+            !range.isEmpty
+        else
+        {
+            return Self.shrink(self, toward: 0)
+        }
+        
+        let target: Self
+        
+        if range.contains(0)
+        {
+            target = 0
+        }
+        else if 0 < range.lowerBound
+        {
+            target = range.lowerBound
+        }
+        else
+        {
+            target = range.upperBound
+        }
+        
+        return Self.shrink(self, toward: target)
+            .filter { range.contains($0) }
+    }
+    
+    
+    
+    /// Shrinks the given value toward the given target by repeatedly halving
+    /// the distance.
+    /// - Parameters:
+    ///   - value: The value to shrink.
+    ///   - target: The value toward which shrink candidates converge.
+    /// - Returns: The shrink candidates.
+    private static func shrink(
+        _       value   : Self,
+        toward  target  : Self
+    ) -> [Self]
+    {
+        guard value != target
         else
         {
             return []
         }
         
-        var candidates  : [Self]    = [0]
-        var diff        : Self      = self
+        var candidates  : [Self]    = [target]
+        var distance    : Self      = value - target
         
         while true
         {
-            diff /= 2
+            distance /= 2
             
-            if diff == 0
+            if distance == 0
             {
                 break
             }
             
-            candidates.append(self - diff)
+            candidates.append(value - distance)
         }
         
         return candidates
