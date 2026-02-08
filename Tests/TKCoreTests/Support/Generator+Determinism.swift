6f16cb407fb53a444654ccb90cda2302d30398a1
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-02-08 09:32:45 -0800

    Add `Generator.validateDeterminism()` test method

---
+++ Tests/TKCoreTests/Support/Generator+Determinism.swift
@@ -0,0 +1,30 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-test-kit open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+import XCTest
+@testable import TestKitBase
+
+
+
+internal extension Generator where V : Equatable
+{
+    /// Validates that output of the generator is deterministic.
+    func validateDeterminism()
+    {
+        for _ in 0..<1000
+        {
+            let (context1, context2) = GenerationContext.sameRandomContexts
+            
+            XCTAssertEqual(
+                self.generate(context1),
+                self.generate(context2)
+            )
+        }
+    }
+}
