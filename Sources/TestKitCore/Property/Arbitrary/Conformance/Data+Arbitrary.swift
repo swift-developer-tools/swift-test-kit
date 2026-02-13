87edb7887419b4c7502a32198b6dc668b5b31d5a
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-02-13 12:57:17 -0800

    Add `Data` `Arbitrary` conformance

---
+++ Sources/TestKitCore/Property/Arbitrary/Conformance/Data+Arbitrary.swift
@@ -0,0 +1,52 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-test-kit open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+import Foundation
+
+
+
+extension Data: Arbitrary
+{
+    /// Generates an arbitrary value using the given generation context.
+    /// - Parameter context: The generation context.
+    /// - Returns: An arbitrary data value with a count in the range
+    /// `0...context.size`, filled with arbitrary bytes.
+    public static func arbitrary(
+        using context: GenerationContext
+    ) -> Data
+    {
+        let count: Int = context.random(in: 0...context.size)
+        
+        let bytes: [UInt8] = (0..<count).map
+        {
+            _ in
+            
+            return UInt8.arbitrary(using: context)
+        }
+        
+        return Data(
+            bytes:  bytes,
+            count:  count
+        )
+    }
+    
+    
+    
+    /// Generates candidate values that are smaller than the receiver value.
+    ///
+    /// Candidates are produces by converting to an array of bytes, shrinking
+    /// that array, and converting back to data.
+    ///
+    /// - Returns: An array of smaller candidate values, or an empty array
+    /// to indicate that no shrinking should occur.
+    public func shrink() -> [Data]
+    {
+        return Array(self).shrinkTowardEmpty().map { Data($0) }
+    }
+}
