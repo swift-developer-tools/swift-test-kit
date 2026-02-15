fd22e2c29c853df0dab472fb30b70fd69db6f012
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-02-15 10:56:15 -0800

    Add `GenerationContext` random utility methods
    Add methods to concisely create a `GenerationContext` with a random
    seed or a random size, but not both.

--- Tests/TestKitTests/Support/Property/GenerationContext+Random.swift
+++ Tests/TestKitTests/Support/Property/GenerationContext+Random.swift
@@ -25,12 +25,42 @@ extension GenerationContext
     
     
     
-    /// A random generation context with a random seed and zero size.
+    /// A generation context with a random seed and zero size.
     internal static var randomZeroSize: GenerationContext
     {
         return GenerationContext(
             seed:   randomSeed,
             size:   0
+        )
+    }
+    
+    
+    
+    /// A generation context with a random seed and the given size.
+    /// - Parameter size: The size to use.
+    /// - Returns: A generation context with a random seed and the given size.
+    internal static func randomSeed(
+        size: Int
+    ) -> GenerationContext
+    {
+        return GenerationContext(
+            seed:   randomSeed,
+            size:   size
+        )
+    }
+    
+    
+    
+    /// A generation context with a random size and the given seed.
+    /// - Parameter seed: The seed to use.
+    /// - Returns: A generation context with a random size and the given seed.
+    internal static func randomSize(
+        seed: UInt64
+    ) -> GenerationContext
+    {
+        return GenerationContext(
+            seed:   seed,
+            size:   randomSize
         )
     }
     
