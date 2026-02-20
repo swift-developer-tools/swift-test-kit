34d4f486eb2647702f41112e687357c52716ae41
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-02-20 08:16:07 -0800

    Add `Generator.sample(count:seed:maxSize:)`

--- Sources/TestKitCore/Property/Generation/Generator+Convenience.swift
+++ Sources/TestKitCore/Property/Generation/Generator+Convenience.swift
@@ -26,6 +26,55 @@ extension Generator where V : Arbitrary
 
 
 
+// MARK: - Sample
+
+extension Generator
+{
+    /// Generates samples values.
+    ///
+    /// Use this to verify that a custom generator produces the expected
+    /// distribution of values.
+    ///
+    /// - Parameters:
+    ///   - count: The number of values to generate. The default value is `10`.
+    ///   - seed: The seed used to initialize the random number generator.
+    ///   The default value is `nil`, which generates a random seed from the
+    ///   system random number generator.
+    ///   - maxSize: The maximum generation size. The default value is `100`.
+    /// - Returns: The sample values.
+    public func sample(
+        count   : Int       = 10,
+        seed    : UInt64?   = nil,
+        maxSize : Int       = 100
+    ) -> [V]
+    {
+        precondition(
+            count >= 0,
+            "count must be non-negative"
+        )
+        
+        precondition(
+            maxSize >= 0,
+            "maxSize must be non-negative"
+        )
+        
+        let seed: UInt64 = seed ?? .random(in: UInt64.min...UInt64.max)
+        
+        let context = GenerationContext(seed: seed)
+        
+        return (0..<count).map
+        {
+            context.size = count > 0
+                ? $0 * maxSize / count
+                : 0
+            
+            return generate(context)
+        }
+    }
+}
+
+
+
 // MARK: - Array
 
 extension Generator
