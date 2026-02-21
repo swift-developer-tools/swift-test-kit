6988a9bb0c5ae4594292f128b307bf79ad916311
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-02-21 15:57:20 -0800

    Check `maxCommandCount` in `OptionsTests`

--- Tests/TestKitTests/TestKitCore/OptionsTests.swift
+++ Tests/TestKitTests/TestKitCore/OptionsTests.swift
@@ -81,6 +81,7 @@ internal final class OptionsTests: TestKitCase
         XCTAssertEqual(options.maxShrinkSteps, 100)
         XCTAssertEqual(options.maxSize, 100)
         XCTAssertEqual(options.maxDiscardRatio, 10)
+        XCTAssertEqual(options.maxCommandCount, 100)
         XCTAssertNil(options.seed)
     }
     
