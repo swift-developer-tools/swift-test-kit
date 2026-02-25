2640d92c4354141b2c293adbb348dfd15ad26944
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-02-25 14:36:48 -0800

    Test `statistics` in `OptionsTests.testPropertyOptions()`

--- Tests/TestKitTests/TestKitCore/OptionsTests.swift
+++ Tests/TestKitTests/TestKitCore/OptionsTests.swift
@@ -82,6 +82,7 @@ internal final class OptionsTests: TestKitCase
         XCTAssertEqual(options.maxSize, 100)
         XCTAssertEqual(options.maxDiscardRatio, 10)
         XCTAssertEqual(options.maxCommandCount, 100)
+        XCTAssertEqual(options.statistics, [])
         XCTAssertNil(options.seed)
     }
     
