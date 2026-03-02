98a17b7db62b60073737f283c9829f1d522646ae
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-03-02 09:15:54 -0800

    Add performance evaluator test mirrors

---
+++ Tests/TestKitTests/Support/Mirrors/Performance+Mirrors.swift
@@ -0,0 +1,47 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-test-kit open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+import TestKitCore
+@testable import XCTestKit
+
+
+
+/// This function is a mirror wrapping the actual internal function,
+/// allowing tests to benefit from autoclosures and default parameters.
+/// The XCTestKit failure context is used since tests are run with XCTest.
+internal func TKPerformance(
+    runs        : @autoclosure () -> Int?       = nil,
+    warmupRuns  : @autoclosure () -> Int?       = nil,
+    timeLimit   : @autoclosure () -> Duration?  = nil,
+    memoryLimit : @autoclosure () -> UInt64?    = nil,
+    _ message   : @autoclosure () -> String     = "",
+    fileID      : StaticString                  = #fileID,
+    file        : StaticString                  = #filePath,
+    line        : UInt                          = #line,
+    column      : UInt                          = #column,
+    options     : TestOptions?                  = nil,
+    context     : FailureContext                = XCTestKit.failureContext,
+    _ body      : () async throws -> Void
+) async
+{
+    await TestKitCore.TKPerformance(
+        runs:           runs,
+        warmupRuns:     warmupRuns,
+        timeLimit:      timeLimit,
+        memoryLimit:    memoryLimit,
+        message,
+        fileID:         fileID,
+        file:           file,
+        line:           line,
+        column:         column,
+        options:        options ?? TestConfiguration.global,
+        body,
+        context:        context
+    )
+}
