ad48fb9f64883f0f4564a610ffadf5bcfa748fbf
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-03-01 16:24:40 -0800

    Add `TKAssertPerformance()`

---
+++ Sources/TestKitCore/Performance/Performance.swift
@@ -0,0 +1,52 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the swift-test-kit open source project.
+//
+// Copyright (c) Margins Technologies LLC.
+// Licensed under the Apache License, Version 2.0.
+//
+//===----------------------------------------------------------------------===//
+
+package func TKAssertPerformance(
+    runs        : () -> Int?,
+    warmupRuns  : () -> Int?,
+    timeLimit   : () -> Duration?,
+    memoryLimit : () -> UInt64?,
+    _ message   : () -> String,
+    fileID      : StaticString,
+    file        : StaticString,
+    line        : UInt,
+    column      : UInt,
+    options     : TestOptions,
+    _ body      : () async throws -> Void,
+    context     : FailureContext
+) async
+{
+    let perfOptions: PerformanceOptions = options.performanceOptions
+    
+    let runs        : Int           = runs()        ?? perfOptions.runs
+    let warmupRuns  : Int           = warmupRuns()  ?? perfOptions.warmupRuns
+    let timeLimit   : Duration?     = timeLimit()   ?? perfOptions.timeLimit
+    let memoryLimit : UInt64?       = memoryLimit() ?? perfOptions.memoryLimit
+    
+    let result: PerformanceResult = await PerformanceRunner.run(
+        runs:           runs,
+        warmupRuns:     warmupRuns,
+        timeLimit:      timeLimit,
+        memoryLimit:    memoryLimit,
+        body:           body
+    )
+    
+    result.emit(
+        runs:           runs,
+        warmupRuns:     warmupRuns,
+        functionName:   "\(context.framework.rawValue)AssertPerformance",
+        options:        options,
+        context:        context,
+        message:        message,
+        fileID:         fileID,
+        file:           file,
+        line:           line,
+        column:         column
+    )
+}
