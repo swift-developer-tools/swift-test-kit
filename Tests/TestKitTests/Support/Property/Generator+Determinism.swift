16b6fe32d7033b34071b23d6eff556334e29e77d
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-03-04 13:31:29 -0800

    Add `size` parameter to `Generator.assertDeterministic()`

--- Tests/TestKitTests/Support/Property/Generator+Determinism.swift
+++ Tests/TestKitTests/Support/Property/Generator+Determinism.swift
@@ -15,11 +15,28 @@ import XCTest
 extension Generator where V : Equatable
 {
     /// Asserts that output of the generator is deterministic.
-    internal func assertDeterministic()
+    /// - Parameter size: The generation size to use. The default value is
+    /// `nil`, which falls back to using random sizes.
+    internal func assertDeterministic(
+        size: Int? = nil
+    )
     {
         for _ in 0..<1000
         {
-            let (context1, context2) = GenerationContext.sameRandomContexts
+            let context1    : GenerationContext
+            let context2    : GenerationContext
+            
+            if let size
+            {
+                let seed: UInt64 = GenerationContext.randomSeed
+                
+                context1    = GenerationContext(seed: seed, size: size)
+                context2    = GenerationContext(seed: seed, size: size)
+            }
+            else
+            {
+                (context1, context2) = GenerationContext.sameRandomContexts
+            }
             
             let value1  : V     = self.generate(context1)
             let value2  : V     = self.generate(context2)
