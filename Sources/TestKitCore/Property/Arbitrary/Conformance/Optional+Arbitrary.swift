699981adcd577e2eae7bdcaa3220562a1df052bb
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-03-06 13:33:32 -0800

    Add `Optional` `Arbitrary` mutation override

--- Sources/TestKitCore/Property/Arbitrary/Conformance/Optional+Arbitrary.swift
+++ Sources/TestKitCore/Property/Arbitrary/Conformance/Optional+Arbitrary.swift
@@ -47,4 +47,35 @@ extension Optional: Arbitrary where Wrapped : Arbitrary
                 return [nil] + wrapped.shrink().map { .some($0) }
         }
     }
+    
+    
+    
+    /// Produces a value that is a small perturbation of the receiver value.
+    /// - Parameter context: The generation context.
+    /// - Returns: A mutated optional value.
+    public func mutate(
+        using context: GenerationContext
+    ) -> Optional
+    {
+        /// If `nil`, always return some non-`nil` value, since `nil` is a
+        /// degenerate case that can always be reached via exploration.
+        /// Getting back to a meaningful value from `nil` is more useful.
+        /// Otherwise, if non-`nil`, there is a 10% chance of returning `nil`.
+        
+        switch self
+        {
+            case .none:
+                
+                return Wrapped.arbitrary(using: context)
+                
+            case let .some(wrapped):
+                
+                if context.random(in: 1...10) == 1
+                {
+                    return nil
+                }
+                
+                return wrapped.mutate(using: context)
+        }
+    }
 }
