e8b9e85f9c55bfd3b422fab06c74de58a3da2206
swift-developer-tools <229857922+swift-developer-tools@users.noreply.github.com>
2026-03-10 13:51:34 -0700

    Add `size` parameter to `Generator.assertCount(in:)`

--- Tests/TestKitTests/Support/Property/Generator+Count.swift
+++ Tests/TestKitTests/Support/Property/Generator+Count.swift
@@ -16,18 +16,23 @@ extension Generator where V : Collection
 {
     /// Asserts that the generator produces collections with counts within
     /// the given range.
-    /// - Parameter expected: The expected range of counts.
+    /// - Parameters:
+    ///   - expected: The expected range of counts.
+    ///   - size: The generation size to use. The default value is `nil`,
+    ///   which falls back to using random sizes.
     internal func assertCount(
-        in expected: ClosedRange<Int>
+        in expected : ClosedRange<Int>,
+        size        : Int?              = nil
     )
     {
         for _ in 0..<1000
         {
-            let value = generate(.random)
+            let value: V = size == nil
+                ? generate(.random)
+                : generate(.randomSeed(size: size!))
             
             XCTAssertGreaterThanOrEqual(value.count, expected.lowerBound)
             XCTAssertLessThanOrEqual(value.count, expected.upperBound)
         }
     }
-
 }
